f58fd77a081240f672d002aaad1c4c1530f511ac
Nate Higueros <78056606+natehigueros@users.noreply.github.com>
2022-11-02 10:58:28 -0600

    Add files via upload

--- xadc.vhd
+++ xadc.vhd
@@ -5,23 +5,27 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity xadc is
-  Port (clk, rst : in std_logic);
+  Port (clk, rst : in std_logic;
+        VAUX1, VAUX2 :in std_logic;
+        LED_OUT : out std_logic_vector(15 downto 0));
 end xadc;
 
 architecture Behavioral of xadc is
 
 COMPONENT xadc_wiz_0
   PORT (
-    di_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0); --data in
+    di_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
     daddr_in : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
     den_in : IN STD_LOGIC;
     dwe_in : IN STD_LOGIC;
     drdy_out : OUT STD_LOGIC;
-    do_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0); --data out
+    do_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
     dclk_in : IN STD_LOGIC;
     reset_in : IN STD_LOGIC;
     vp_in : IN STD_LOGIC;
     vn_in : IN STD_LOGIC;
+    vauxp6 : IN STD_LOGIC;
+    vauxn6 : IN STD_LOGIC;
     user_temp_alarm_out : OUT STD_LOGIC;
     vccint_alarm_out : OUT STD_LOGIC;
     vccaux_alarm_out : OUT STD_LOGIC;
@@ -36,13 +40,13 @@ END COMPONENT;
 
 signal    di_in :  STD_LOGIC_VECTOR(15 DOWNTO 0):= x"0000";
 signal    den_in :  STD_LOGIC;
-signal    dwe_in :  STD_LOGIC:= '0';
+signal    dwe_in :  STD_LOGIC;
 signal    drdy_out :  STD_LOGIC;
 signal    do_out :  STD_LOGIC_VECTOR(15 DOWNTO 0);
 signal    dclk_in :  STD_LOGIC;
 signal    reset_in :  STD_LOGIC;
-signal    vp_in :  STD_LOGIC:= '0';
-signal    vn_in :  STD_LOGIC:= '0';
+signal    vp_in :  STD_LOGIC;
+signal    vn_in :  STD_LOGIC;
 signal    user_temp_alarm_out :  STD_LOGIC;
 signal    vccint_alarm_out :  STD_LOGIC;
 signal    vccaux_alarm_out :  STD_LOGIC;
@@ -52,40 +56,37 @@ signal    eoc_out :  STD_LOGIC;
 signal    alarm_out :  STD_LOGIC;
 signal    eos_out : STD_LOGIC;
 signal    busy_out : STD_LOGIC;
-signal    daddr_in :  STD_LOGIC_VECTOR(6 DOWNTO 0):= "00"&channel_out;
+signal    vauxp6 : STD_LOGIC;
+signal    vauxn6 : STD_LOGIC;
+signal    daddr_in :  STD_LOGIC_VECTOR(6 DOWNTO 0);
 
 
 begin
 --top: xadc port map(clk=>clk_s, rst=> rst_s);
-
-LAB4 : xadc_wiz_0
+your_instance_name : xadc_wiz_0
   PORT MAP (
-    di_in => di_in,
+    di_in => x"0000",
     daddr_in => daddr_in,
-    den_in => den_in,
-    dwe_in => dwe_in,
+    den_in => eoc_out,
+    dwe_in => '0',
     drdy_out => drdy_out,
     do_out => do_out,
-    dclk_in => dclk_in,
-    reset_in => reset_in,
-    vp_in => vp_in,
-    vn_in => vn_in,
+    dclk_in => clk,
+    reset_in => rst,
+    vp_in => '0',
+    vn_in => '0',
+    vauxp6 => VAUX1,
+    vauxn6 => VAUX2,
     user_temp_alarm_out => user_temp_alarm_out,
     vccint_alarm_out => vccint_alarm_out,
     vccaux_alarm_out => vccaux_alarm_out,
     ot_out => ot_out,
     channel_out => channel_out,
-    eoc_out => den_in,
+    eoc_out => eoc_out,
     alarm_out => alarm_out,
     eos_out => eos_out,
     busy_out => busy_out
   );
-process(clk,rst)
-begin
-    if rst = '1' then
-        there is issue here
-    elsif ot_out = '1' then
-        
-    end if;
-end process;
+  daddr_in <= "00"&channel_out;
+  LED_OUT <= do_out;
 end Behavioral;
---
+++ xadc_wiz_0/xadc_wiz_0_stub.vhdl
@@ -0,0 +1,48 @@
+-- Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+-- Date        : Wed Nov  2 09:08:53 2022
+-- Host        : ET133C07-025344 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/nicholashigueros/project_1/project_1.srcs/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
+-- Design      : xadc_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xadc_wiz_0 is
+  Port ( 
+    daddr_in : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    den_in : in STD_LOGIC;
+    di_in : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dwe_in : in STD_LOGIC;
+    do_out : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    drdy_out : out STD_LOGIC;
+    dclk_in : in STD_LOGIC;
+    reset_in : in STD_LOGIC;
+    vauxp6 : in STD_LOGIC;
+    vauxn6 : in STD_LOGIC;
+    busy_out : out STD_LOGIC;
+    channel_out : out STD_LOGIC_VECTOR ( 4 downto 0 );
+    eoc_out : out STD_LOGIC;
+    eos_out : out STD_LOGIC;
+    ot_out : out STD_LOGIC;
+    vccaux_alarm_out : out STD_LOGIC;
+    vccint_alarm_out : out STD_LOGIC;
+    user_temp_alarm_out : out STD_LOGIC;
+    alarm_out : out STD_LOGIC;
+    vp_in : in STD_LOGIC;
+    vn_in : in STD_LOGIC
+  );
+
+end xadc_wiz_0;
+
+architecture stub of xadc_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "daddr_in[6:0],den_in,di_in[15:0],dwe_in,do_out[15:0],drdy_out,dclk_in,reset_in,vauxp6,vauxn6,busy_out,channel_out[4:0],eoc_out,eos_out,ot_out,vccaux_alarm_out,vccint_alarm_out,user_temp_alarm_out,alarm_out,vp_in,vn_in";
+begin
+end;
