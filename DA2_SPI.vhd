d041b011e73da5f17b3e42fa59d5909f324feeac
Jack <jackfernald@mail.weber.edu>
2022-11-16 14:42:26 -0700

    Spi and constraints spi

--- DA2_SPI.vhd
+++ DA2_SPI.vhd
@@ -8,86 +8,95 @@ entity DA2_SPI is
 -- spi_clk_f is limited to 30 MHz for DA2
 generic(m_clk_f : in integer := 100e6;
             spi_clk_f : in integer := 10e6);
-port ( clk : in std_logic; -- clock input
-reset : in std_logic; -- reset, active high
-load : in std_logic; -- notification to send data
-data_in : in std_logic_vector(15 downto 0); -- pdata in
-sdata_0 : out std_logic; -- serial data out 1
-sdata_1 : out std_logic; -- serial data out 2
-spi_clk : out std_logic; -- clk out to SPI devices
-CS0_n : out std_logic); -- chip select 1, active low
+            port ( clk : in std_logic; -- clock input
+            reset : in std_logic; -- reset, active high
+            load : in std_logic; -- notification to send data
+            data_in : in std_logic_vector(15 downto 0); -- pdata in
+            sdata_0 : out std_logic; -- serial data out 1
+            sdata_1 : out std_logic; -- serial data out 2
+            spi_clk : out std_logic; -- clk out to SPI devices
+            CS0_n : out std_logic; -- chip select 1, active low
+            is_busy : out std_logic);
 end DA2_SPI;
 
 architecture Behavioral of DA2_SPI is
 
-signal baud_timer : integer := m_clk_f/spi_clk_f;
-signal baud_load : std_logic := '0';
-signal baud_load_val : integer;
+constant FULL_COUNT : integer:= (m_clk_f/spi_clk_f)/2;
+signal clock_timer : integer := (m_clk_f/spi_clk_f)/2;
+signal clock_load : std_logic := '0';
+constant clock_load_val : integer := FULL_COUNT;
 signal BIT_COUNT : integer := 0;
 signal bit_rst : std_logic := '0';
 signal BIT_En : std_logic := '0';
+signal pdata_buffer : std_logic_vector(15 downto 0);
 
 type STATES is (idle, tx1, tx2);
 
 signal state_spi :STATES := idle;
 signal flag_out: std_logic := '0';
-constant FULL_COUNT : integer:= 100e6 / 115200;
-constant HALF_COUNT : integer:= FULL_COUNT/2;
+
+
 
 begin
-baud_load_val <= HALF_COUNT when STATE_SPI = IDLE else FULL_COUNT;  --What is Half_count and Full_count
+--clock_load_val <= FULL_COUNT when STATE_SPI = IDLE else FULL_COUNT;  --What is Half_count and Full_count
 CS0_n <= '0' when state_spi = tx1  or state_spi = tx2 else '1';
+spi_clk <= '0' when state_spi = tx2 else '1';
+sdata_0 <= pdata_buffer(bit_count) when bit_count >= 0;
+sdata_1 <= pdata_buffer(bit_count) when bit_count >= 0;
+is_busy <= '1' when state_spi /= IDLE else '0';
+bit_rst <= '1' when state_spi = IDLE else '0';
+
 --Case Process
 process(clk,reset)
 begin
-    if reset='1' then 
+    if reset='1' then
         state_spi <= IDLE;
-       CS0_n<='1';
-    elsif rising_edge(clk) then
+       --CS0_n<='1';
+    elsif falling_edge(clk) then
         case state_spi is
             when IDLE=>
-               if flag_out = '1' then
-                    CS0_n <= '1';
-               else
-                    flag_out<= '0';
                if load = '1' then
-                    state_spi<=tx1;
-               else 
-                    state_spi<=IDLE;
+                    pdata_buffer <= data_in;
+                    state_spi <= tx1;
+                    clock_load <= '1';
+               else
+                    state_spi <= idle;
                end if;
-               end if;
-               
             when tx1=>
-                --This state should cause sclk to go high
-                --This state should be the buffer function so load stuff in
-               
-                if something then
-                    something
+                bit_en <= '0';
+                if clock_timer > 0 then
+                    state_spi <= tx1;
+                    clock_load <= '0';
                 else
-                    something
+                    state_spi <= tx2;
+                    clock_load <= '1';
                 end if;
             when tx2=>
-            --This state should cause sclk to go low
-                if something then
-                    something
+                if clock_timer <= 0 and bit_count > 0 then
+                    state_spi <= tx1;
+                    bit_en <= '1';
+                    clock_load <= '1';
+                elsif bit_count <= 0 and clock_timer <= 0 then
+                    state_spi <= idle;
                 else
-                    something
+                    state_spi <= tx2;
+                    clock_load <= '0';
                 end if;
         end case;
     end if;
 end process;
---Baud counter
+--Clock counter
 process (clk, reset)
 begin
     if reset = '1' then
-        baud_timer <= HALF_COUNT;
+        clock_timer <= FULL_COUNT;
     elsif falling_edge(clk) then
-        if baud_load = '1' then
-            baud_timer <= baud_load_val;
-        elsif baud_timer > 0 then
-            baud_timer <= baud_timer - 1;
+        if clock_load = '1' then
+            clock_timer <= clock_load_val;
+        elsif clock_timer > 0 then
+            clock_timer <= clock_timer - 1;
         else
-            baud_timer <= 0; -- prevents baud timer from going negative
+            clock_timer <= 0; -- prevents baud timer from going negative
         end if;
     end if;
 end process;
@@ -95,15 +104,16 @@ end process;
 process (clk, reset)
 begin
     if reset = '1' then
-        BIT_COUNT <= 0;
+        BIT_COUNT <= 15;
     elsif falling_edge(clk) then
         if bit_rst = '1' then
-            BIT_COUNT <= 0;
-        elsif BIT_COUNT < 8 AND BIT_En = '1' then --BC can't exceed 8
-            BIT_COUNT <= BIT_COUNT + 1;
+            BIT_COUNT <= 15;
+        elsif BIT_COUNT > 0 AND BIT_En = '1' then
+            BIT_COUNT <= BIT_COUNT - 1;
         else
             BIT_COUNT <= BIT_COUNT;
         end if;
     end if;
 end process;
 end Behavioral;
+
